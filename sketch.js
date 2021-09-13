39c932a8a7f5a15a0ec54973def43f412c37a6e3
Sean Darcy <darcys22@gmail.com>
2021-09-13 12:03:33 +1000

    basic template

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  // put setup code here
+  createCanvas(600, 600); 
+  
+  greeny = color('#baf2bb')
+  mint = color('#baf2d8')
+  bluee = color('#bad7f2')
+  pink = color('#f2bac9')
+  yellow = color('#f2e2ba')
+
+  colors = [greeny, mint, bluee, pink, yellow];
+  
+  background(random(colors));
+  
+}
+
+function draw() {
+  // put drawing code here
+}
